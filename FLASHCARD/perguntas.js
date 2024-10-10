864e9af6657870a9b219fef11bfd780b14d22b10
Jao8-o <joaovgromero@gmail.com>
2024-10-10 07:29:00 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'anime',
+    'oque é o one piece?',
+    'ninguem sabe'
+)
+
+criaCartao(
+    'ingles',
+    'como se fala ola mundo em ingles',
+    'hello world'
+)
+
+criaCartao(
+    'atualidades',
+    'quem ganhou a terceira guerra mundial?',
+    'Brasil'
+)
+
+criaCartao(
+    'escolar',
+    'qual o nome da nossa diretora?',
+    'solange'
+)
